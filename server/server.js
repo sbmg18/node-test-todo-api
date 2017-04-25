5aa95d20f4e72e31c9279ff1be82a6110068d6b3
Shubham Gupta <sbmg18@gmail.com>
2017-04-25 14:38:00 +0530

    Add /GET todos

--- server/server.js
+++ server/server.js
@@ -22,6 +22,15 @@ app.post('/todos', (req, res) => {
     });
 });
 
+app.get('/todos', (req, res) => {
+
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (err) => {
+        res.status(400).send(err);
+    });
+});
+
 module.exports = {app};
 
 app.listen(3000, () => {
