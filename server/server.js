59c70e3bb997f948bf80fca9ad8ebc2b0f35b5f0
Shubham Gupta <sbmg18@gmail.com>
2017-04-25 16:08:37 +0530

    Add GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,16 +1,17 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var { ObjectID } = require('mongodb');
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
 
 var app = express();
 
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-    
+
     var newTodo = new Todo({
         text: req.body.text
     });
@@ -25,19 +26,36 @@ app.post('/todos', (req, res) => {
 app.get('/todos', (req, res) => {
 
     Todo.find().then((todos) => {
-        res.send({todos});
+        res.send({ todos });
     }, (err) => {
         res.status(400).send(err);
     });
 });
 
-module.exports = {app};
+app.get('/todos/:id', (req, res) => {
+
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({ todo });
+
+    }).catch((err) => {
+        res.status(400).send();
+    });
+});
 
 app.listen(3000, () => {
     console.log("Server started on port 3000");
 });
 
- 
+module.exports = { app };
 
 
- 
+
